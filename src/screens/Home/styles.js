ee9fa3ed9110d7839b12d6a3b49fafeabffa4c91
MateusOli21 <mateus.oli.abreu@hotmail.com>
2020-06-20 17:32:05 -0300

    Changed title and card title color of home screen.

--- src/screens/Home/styles.js
+++ src/screens/Home/styles.js
@@ -14,12 +14,14 @@ export const Container = styled.View``;
 export const Title = styled.Text`
   font-size: ${fontSizes.h6};
   font-family: ${fonts.montserratBold};
+  color: ${theme.secondary};
   margin-bottom: 20px;
 `;
 
 export const CardTitle = styled.Text`
   font-size: ${fontSizes.p16};
   font-family: ${fonts.montserratBold};
+  color: ${theme.secondary};
   text-align: center;
   padding: 4px 0;
   margin: 0 0 12px;
